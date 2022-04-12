e9e92b7ea276028af3a03d4bcc856d285eeefad6
Fatoş Dilara Fırtına <dilara_firtina@hotmail.com>
2022-04-12 18:20:23 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+var customerName="bob";
+
+const upperCaseCustomerName = () => customerName.toUpperCase()
+console.log(upperCaseCustomerName());
+
+const setBestCustomer = () => {
+    var bestCustomer = "not bob"
+    return bestCustomer
+}
+console.log(setBestCustomer());
+
+const overwriteBestCustomer = () => {
+    bestCustomer = "Dilara"
+    return bestCustomer
+}
+console.log(overwriteBestCustomer());
+
+
+const leastFavoriteCustomer  = "Abc"
+
+const changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = "Abc\' sister"
+    return leastFavoriteCustomer
+}
+console.log(changeLeastFavoriteCustomer());
+
+
+
+
+
